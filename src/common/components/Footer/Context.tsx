5460d7cd018d7c92dd3fc4ff56cf7dd070ead89f
brunodassisf <brunodassis30@gmail.com>
2023-03-06 00:35:14 -0300

    criado componente de Footer,  necessita de avalição se vai usar

---
+++ src/common/components/Footer/Context.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="absolute pb-2 right-4 bottom-0">
+      <div className="text-sm text-gray-500">
+        Desenvolvido por Assisdevelopment - 2023
+      </div>
+    </div>
+  );
+}
