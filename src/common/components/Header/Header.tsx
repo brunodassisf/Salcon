e87db0ea4af4085958655ca8252f24233697691d
brunodassisf <brunodassis30@gmail.com>
2023-03-06 00:35:32 -0300

    ajuste de layout no Header

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -13,9 +13,9 @@ export default function Header() {
   };
 
   return (
-    <header className="bg-white shadow-md">
+    <header className="bg-white">
       <nav
-        className="mx-auto flex max-w-7xl items-center justify-between p-4"
+        className="mx-auto flex max-w-7xl items-center justify-between py-3 lg:px-3"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
@@ -40,7 +40,7 @@ export default function Header() {
         <div className="flex lg:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className=" inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
             onClick={clickToogleMenu}
           >
             <span className="sr-only">Open main menu</span>
@@ -73,7 +73,7 @@ export default function Header() {
           x: "100%",
           transition: { delay: 1, duration: 1 },
         }}
-        className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+        className="fixed border-l inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
       >
         <div className="flex items-center justify-between border-b pb-5">
           <Link href="/">
@@ -88,7 +88,7 @@ export default function Header() {
           </Link>
           <button
             type="button"
-            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            className=" rounded-md p-2.5 text-gray-700"
             onClick={clickToogleMenu}
           >
             <span className="sr-only">Close menu</span>
