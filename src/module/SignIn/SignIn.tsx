36426e032c0b330503686ca4a805ccf68ab626ae
brunodassisf <brunodassis30@gmail.com>
2023-03-06 00:37:10 -0300

    criando modulos de login e cadastro

---
+++ src/module/SignIn/SignIn.tsx
@@ -0,0 +1,53 @@
+import { useRouter } from "next/router";
+
+import Button from "@/common/components/Button";
+import Input from "@/common/components/Input";
+import { useFormik } from "formik";
+
+export default function SignIn() {
+  const router = useRouter();
+
+  const redirectLink = () => {
+    router.push("/gerenciar");
+  };
+
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const handleOnSubmit = (values: any) => {
+    console.log(values);
+  };
+
+  const { values, errors, touched, handleChange, handleSubmit } = useFormik({
+    initialValues: initialValues,
+    onSubmit: handleOnSubmit,
+  });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Input
+        label="E-mail"
+        name="email"
+        className="mb-7"
+        value={values.email}
+        error={touched.email && errors.email ? errors.email : undefined}
+        onChange={handleChange}
+      />
+      <Input
+        label="Senha"
+        name="password"
+        className="mb-7"
+        value={values.password}
+        error={
+          touched.password && errors.password ? errors.password : undefined
+        }
+        onChange={handleChange}
+      />
+      <Button type="submit" className="mt-5 w-full" onClick={redirectLink}>
+        Entrar
+      </Button>
+    </form>
+  );
+}
---
+++ src/module/SignUp/SignUp.tsx
@@ -0,0 +1,79 @@
+import { useRouter } from "next/router";
+
+import Button from "@/common/components/Button";
+import Input from "@/common/components/Input";
+import { useFormik } from "formik";
+
+export default function SignUp() {
+  const router = useRouter();
+
+  const redirectLink = () => {
+    router.push("/");
+  };
+
+  const initialValues = {
+    email: "",
+    name: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const handleOnSubmit = (values: any) => {
+    console.log(values);
+  };
+
+  const { values, errors, touched, handleChange, handleSubmit } = useFormik({
+    initialValues: initialValues,
+    onSubmit: handleOnSubmit,
+  });
+
+  return (
+    <form onSubmit={handleSubmit} className="grid grid-cols-4 md:gap-4">
+      <Input
+        label="Nome"
+        name="name"
+        className="mb-7 col-span-4 md:col-span-2"
+        value={values.name}
+        error={touched.name && errors.name ? errors.name : undefined}
+        onChange={handleChange}
+      />
+      <Input
+        label="E-mail"
+        name="email"
+        className="mb-7 col-span-4 md:col-span-2"
+        value={values.email}
+        error={touched.email && errors.email ? errors.email : undefined}
+        onChange={handleChange}
+      />
+      <Input
+        label="Senha"
+        name="password"
+        className="mb-7 col-span-4 md:col-span-2"
+        value={values.password}
+        error={
+          touched.password && errors.password ? errors.password : undefined
+        }
+        onChange={handleChange}
+      />
+      <Input
+        label="Confirma senha"
+        name="confirmPassword"
+        className="mb-7 col-span-4 md:col-span-2"
+        value={values.confirmPassword}
+        error={
+          touched.confirmPassword && errors.confirmPassword
+            ? errors.confirmPassword
+            : undefined
+        }
+        onChange={handleChange}
+      />
+      <Button
+        type="submit"
+        className="mt-1 w-full col-span-4"
+        onClick={redirectLink}
+      >
+        Cadastrar
+      </Button>
+    </form>
+  );
+}
