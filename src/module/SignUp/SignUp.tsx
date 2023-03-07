a60fec4c401743e6a7b6074dc2d152fc35924310
brunodassisf <brunodassis30@gmail.com>
2023-03-07 01:03:39 -0300

    Adicionando ação para cadastrar usuário

--- src/module/SignUp/SignUp.tsx
+++ src/module/SignUp/SignUp.tsx
@@ -1,13 +1,17 @@
 import { useRouter } from "next/router";
 
-import { useFormik } from "formik";
+import { FormikHelpers, useFormik } from "formik";
 
 import Button from "@/common/components/Button";
 import Input from "@/common/components/Input";
 
 import signupSchema from "./SignUp.validation";
+import Spinner from "@/common/components/Spinner";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 export default function SignUp() {
+  const [loading, isLoading] = useState(false);
   const router = useRouter();
 
   const redirectLink = () => {
@@ -21,8 +25,25 @@ export default function SignUp() {
     confirmPassword: "",
   };
 
-  const handleOnSubmit = (values: any) => {
-    console.log(values);
+  const handleOnSubmit = async (
+    values: any,
+    formikHelpers: FormikHelpers<any>
+  ) => {
+    isLoading(true);
+    const response = await fetch("/api/User", {
+      method: "POST",
+      body: JSON.stringify(values),
+    });
+
+    const data = await response.json();
+
+    toast.success(data.msg);
+
+    if (data.res) {
+      formikHelpers.resetForm({ values: initialValues });
+    }
+
+    isLoading(false);
   };
 
   const { values, errors, touched, handleChange, handleSubmit } = useFormik({
@@ -32,56 +53,59 @@ export default function SignUp() {
   });
 
   return (
-    <form onSubmit={handleSubmit} className="grid grid-cols-4 md:gap-4">
-      <Input
-        label="Nome"
-        name="name"
-        className="mb-7 col-span-4 md:col-span-2"
-        value={values.name}
-        error={touched.name && errors.name ? errors.name : undefined}
-        onChange={handleChange}
-      />
-      <Input
-        label="E-mail"
-        name="email"
-        className="mb-7 col-span-4 md:col-span-2"
-        value={values.email}
-        error={touched.email && errors.email ? errors.email : undefined}
-        onChange={handleChange}
-      />
-      <Input
-        label="Senha"
-        name="password"
-        className="mb-7 col-span-4 md:col-span-2"
-        type="password"
-        changeTypePassword
-        value={values.password}
-        error={
-          touched.password && errors.password ? errors.password : undefined
-        }
-        onChange={handleChange}
-      />
-      <Input
-        label="Confirma senha"
-        name="confirmPassword"
-        className="mb-7 col-span-4 md:col-span-2"
-        type="password"
-        changeTypePassword
-        value={values.confirmPassword}
-        error={
-          touched.confirmPassword && errors.confirmPassword
-            ? errors.confirmPassword
-            : undefined
-        }
-        onChange={handleChange}
-      />
-      <Button
-        type="submit"
-        className="mt-1 w-full col-span-4"
-        onClick={redirectLink}
-      >
-        Cadastrar
-      </Button>
-    </form>
+    <>
+      {loading && <Spinner />}
+      <form onSubmit={handleSubmit} className="grid grid-cols-4 md:gap-4">
+        <Input
+          label="Nome"
+          name="name"
+          className="mb-7 col-span-4 md:col-span-2"
+          value={values.name}
+          error={touched.name && errors.name ? errors.name : undefined}
+          onChange={handleChange}
+        />
+        <Input
+          label="E-mail"
+          name="email"
+          className="mb-7 col-span-4 md:col-span-2"
+          value={values.email}
+          error={touched.email && errors.email ? errors.email : undefined}
+          onChange={handleChange}
+        />
+        <Input
+          label="Senha"
+          name="password"
+          className="mb-7 col-span-4 md:col-span-2"
+          type="password"
+          changeTypePassword
+          value={values.password}
+          error={
+            touched.password && errors.password ? errors.password : undefined
+          }
+          onChange={handleChange}
+        />
+        <Input
+          label="Confirma senha"
+          name="confirmPassword"
+          className="mb-7 col-span-4 md:col-span-2"
+          type="password"
+          changeTypePassword
+          value={values.confirmPassword}
+          error={
+            touched.confirmPassword && errors.confirmPassword
+              ? errors.confirmPassword
+              : undefined
+          }
+          onChange={handleChange}
+        />
+        <Button
+          type="submit"
+          className="mt-1 w-full col-span-4"
+          onClick={redirectLink}
+        >
+          Cadastrar
+        </Button>
+      </form>
+    </>
   );
 }
